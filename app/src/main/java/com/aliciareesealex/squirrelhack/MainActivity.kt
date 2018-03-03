0fc83c978ad6482c96757374393310b6600e647f
Reese Wells <ducoterra@gmail.com>
2018-03-03 16:14:02 -0500

    Adds Event.java

--- app/src/main/java/com/aliciareesealex/squirrelhack/MainActivity.kt
+++ app/src/main/java/com/aliciareesealex/squirrelhack/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+    // private lateinit var firebaseAnalyticsService: FirebaseAnalytics
     private lateinit var firebaseAnalyticsService: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
