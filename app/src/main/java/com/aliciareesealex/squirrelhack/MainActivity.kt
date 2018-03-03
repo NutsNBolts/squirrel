1ca20c60af46350e7a1f1c3b7d93954dfd6d380e
Alex Oladele <dragid10@gmail.com>
2018-03-03 14:09:52 -0500

    Add firebase initializaiton

--- app/src/main/java/com/aliciareesealex/squirrelhack/MainActivity.kt
+++ app/src/main/java/com/aliciareesealex/squirrelhack/MainActivity.kt
@@ -2,11 +2,19 @@ package com.aliciareesealex.squirrelhack
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalyticsService: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFirebaseStuff()
+    }
+
+    private fun initFirebaseStuff() {
+        firebaseAnalyticsService = FirebaseAnalytics.getInstance(this)
+
     }
 }
