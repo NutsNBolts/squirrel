543717585dc07c65ba0eb91d978e948a8a00855c
fitchae <fitchae@miamioh.edu>
2018-03-03 15:39:31 -0500

    Creates a daily data with a tab layout

---
+++ app/src/main/java/com/aliciareesealex/squirrelhack/DailyDataFragment.java
@@ -0,0 +1,29 @@
+package com.aliciareesealex.squirrelhack;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DailyDataFragment extends Fragment {
+
+
+    public DailyDataFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_daily_data, container, false);
+    }
+
+}
